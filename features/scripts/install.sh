6363ffa4ce4cf0b03f182dfa5b5c6b4499df7591
D-Andreev <d.andreev90@gmail.com>
2016-05-01 12:45:15 +0300

    WIP

--- features/scripts/install.sh
+++ features/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mysql-server
 /etc/init.d/mysql start
+mysql -u root -p users < ./features/scripts/database.sql
